a3d9d5dc69c31db2e87c2645c29dd3205558840f
yusuparipin30 <yusuparipin30@gmail.com>
2022-08-14 22:57:43 +0700

    first commit

---
+++ book.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+
+<div class="heading" style="background:url(image/header-bg-3.png) no-repeat">
+   <h1>book now</h1>
+</div>
+
+
+
+<!-- footer section starts -->
+<section class="footer">
+<div class="box-container">
+   <div class="box">
+      <h3>quick links</h3>
+      <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+      <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+      <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+      <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+   </div>
+
+   <div class="box">
+      <h3>extra links</h3>
+      <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+      <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+      <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+      <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+   </div>
+
+   <div class="box">
+      <h3>contact info</h3>
+      <a href="#"><i class="fas fa-phone"></i>0857-4453-6754</a>
+      <a href="#"><i class="fas fa-phone"></i>0896-0987-6473</a>
+      <a href="#"><i class="fas fa-envelope"></i>yusup@gmail.com</a>
+      <a href="#"><i class="fas fa-map"></i>cikarang, 13477</a>
+   </div>
+
+   <div class="box">
+      <h3>follow us</h3>
+      <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+      <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+      <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+      <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+   </div>
+</div>
+
+
+<div class="credit">created by <span>yusuf. web designer</span> | all rights reserved!</div>
+
+
+
+</section>
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="CSS/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section end -->
+
+<!-- home section start -->
+
+<section class="home">
+   <div class="home-slider">
+      <div class="w">
+         <div class="slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+            <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>travel arround the world</h3>
+            <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+            <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>discover the new places</h3>
+            <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+            <div class="content">
+            <span>explore, discover, travel</span>
+            <h3>make yout ture worthwhile</h3>
+            <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+      </div>
+   </div>
+</section>
+
+<!-- home section end -->
+
+
+<!-- service section start -->
+
+<section class="services">
+
+<h1 class="heading-title">layanan kami</h1>
+
+<div class="box-container">
+
+<div class="box">
+   <img src="images/icon-1.png" alt="">
+   <h3>adventures</h3>
+</div>
+
+<div class="box">
+   <img src="images/icon-2.png" alt="">
+   <h3>tour guide</h3>
+</div>
+
+<div class="box">
+   <img src="images/icon-3.png" alt="">
+   <h3>trekking</h3>
+</div>
+
+<div class="box">
+   <img src="images/icon-4.png" alt="">
+   <h3>camp fire</h3>
+</div>
+
+<div class="box">
+   <img src="images/icon-5.png" alt="">
+   <h3>off road</h3>
+</div>
+
+<div class="box">
+   <img src="images/icon-6.png" alt="">
+   <h3>camping</h3>
+</div>
+
+</div>
+</section>
+
+<!-- service section end -->
+
+
+
+<!-- footer section starts -->
+<section class="footer">
+<div class="box-container">
+   <div class="box">
+      <h3>quick links</h3>
+      <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+      <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+      <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+      <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+   </div>
+
+   <div class="box">
+      <h3>extra links</h3>
+      <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+      <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+      <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+      <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+   </div>
+
+   <div class="box">
+      <h3>contact info</h3>
+      <a href="#"><i class="fas fa-phone"></i>0857-4453-6754</a>
+      <a href="#"><i class="fas fa-phone"></i>0896-0987-6473</a>
+      <a href="#"><i class="fas fa-envelope"></i>yusup@gmail.com</a>
+      <a href="#"><i class="fas fa-map"></i>cikarang, 13477</a>
+   </div>
+
+   <div class="box">
+      <h3>follow us</h3>
+      <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+      <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+      <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+      <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+   </div>
+</div>
+
+
+<div class="credit">created by <span>yusuf. web designer</span> | all rights reserved!</div>
+
+
+
+</section>
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
