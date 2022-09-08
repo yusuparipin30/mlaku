c6d9009225a4af3965425374ba441a2a3bc91619
yusuparipin30 <78536188+yusuparipin30@users.noreply.github.com>
2022-09-08 17:15:38 +0700

    Update book_form.php

--- book_form.php
+++ book_form.php
@@ -15,6 +15,10 @@ if(isset($_POST['send'])){
     $request = " insert into book_form(name, email, phone, address, location, guests, arrivals, leaving) 
     values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving')";
 
+
+    //Fungsi mysql_query dalam PHP adalah untuk menjalankan perintah atau instruksi query ke database MySQL/
+    //dimana mysql_query() merupakan sarana dasar yang akan menampung jenis query apapun di dalam nya. 
+    //Jenis query tersebut yang dapat dijalankan di mysql_query() antara lain adalah INSERT, SELECT, UPDATE, dan DELETE atau dalam bahasa standar fitur sebuah program dikenal dengan istilah CRUD. Fungsi mysql_query dapat juga dikatakan sebagai fungsi untuk menampung sebuah perintah atau intruksi ke database MySQL baik dalam PHP atau bahasa pemrograman lain.
     mysqli_query($connection, $request);
 
     header('location:book.php');
